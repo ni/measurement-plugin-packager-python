c9bac766d2349c57c779775c056119a498ce6c99
divakar-selvam <divakar.selvam@solitontech.com>
2024-11-18 19:13:07 +0530

    chore: rename main.py to start.py

--- src/ni_measurement_plugin_package_builder/__main__.py
+++ src/ni_measurement_plugin_package_builder/__main__.py
@@ -2,6 +2,6 @@
 python measurement plug-ins and uploading it to SystemLink Feeds.
 """
 
-from ni_measurement_plugin_package_builder.main import run
+from ni_measurement_plugin_package_builder.start import run
 
 run()
